080d12c391759c290a39c1e4817c99b66316ee15
Joker-Bat <shanmugam091098@gmail.com>
2022-11-04 14:13:40 +0530

    Made the Navbar collapsable for small screens

--- layouts/HomeLayout/HomeLayout.tsx
+++ layouts/HomeLayout/HomeLayout.tsx
@@ -1,4 +1,7 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState, useRef, useEffect } from "react";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { useRouter } from "next/router";
+
 import SideBar from "../../components/SideBar/SideBar";
 import { getLayout as getSiteLayout } from "../RootLayout/RootLayout";
 import styles from "./HomeLayout.module.scss";
@@ -8,10 +11,42 @@ interface Props {
 }
 
 const HomeLayout = ({ children }: Props) => {
+  const router = useRouter();
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const navBarRef = useRef<HTMLElement | null>(null);
+
+  // Close navbar if we clicked outside of navbar
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (navBarRef.current && !navBarRef.current.contains(e.target as Node)) {
+        setIsNavExpanded(false);
+      }
+    };
+    window.addEventListener("click", handler);
+
+    return () => {
+      window.removeEventListener("click", handler);
+    };
+  }, []);
+
+  // Close navbar if url changed
+  useEffect(() => {
+    setIsNavExpanded(false);
+  }, [router.pathname]);
+
   return (
     <div className={styles.Container}>
-      <aside className={styles.Sidebar}>
+      <aside
+        ref={navBarRef}
+        className={`${styles.Sidebar} ${isNavExpanded ? styles.Active : ""}`}
+      >
         <SideBar />
+        <span
+          className={styles.ExpandIcon}
+          onClick={() => setIsNavExpanded((p) => !p)}
+        >
+          <BsFillArrowRightCircleFill />
+        </span>
       </aside>
       <section className={styles.Content}>{children}</section>
     </div>
